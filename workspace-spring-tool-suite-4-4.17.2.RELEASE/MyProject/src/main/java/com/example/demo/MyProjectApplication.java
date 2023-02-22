c2fd186b8c8733d9b59739b41fa4ba348d6d0df8
Ragu055 <120321329+Ragu055@users.noreply.github.com>
2023-02-22 12:17:48 +0530

    Add files via upload

---
+++ workspace-spring-tool-suite-4-4.17.2.RELEASE/MyProject/src/main/java/com/example/demo/MyProjectApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class MyProjectApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context=SpringApplication.run(MyProjectApplication.class, args);
+		
+	Home h= context.getBean(Home.class);
+	h.contect();
+	//been of spring
+	Home h2= context.getBean(Home.class);
+	h2.contect();
+	}
+
+}
